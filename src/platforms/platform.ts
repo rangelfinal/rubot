d4d39ebd2da7cfcc8b156dd813c09f1ec46d0c9a
Vitor Rangel <rangel.final@gmail.com>
2017-10-20 19:32:26 -0200

    Remover botly

--- src/platforms/platform.ts
+++
@@ -1,8 +0,0 @@
-import MenuContents from '../restaurants/menuContents';
-
-export default abstract class Platform {
-  public abstract sendText(text: string, target: string): Promise<any>;
-  public abstract sendRichMessage(type: string, payload: any, target: string): Promise<any>;
-
-  public abstract sendMenuContents(menuContents: MenuContents, target: string);
-}
